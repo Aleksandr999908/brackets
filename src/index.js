9d10eab50e174d3fe35a1ab67ff7e34cabef1037
github-Aleksandr999908 <aleksandr999908@mail.ru>
2023-01-22 10:23:02 +0500

    add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr1 = bracketsConfig.map((item) => {
+    return `${item[0]}${item[1]}`;
+  });
+
+  let arr2 = str.length;
+
+  while (str.length) {
+    arr1.forEach((item) => {
+      str = str.replaceAll(item, "");
+    });
+
+    if (str.length === arr2) {
+      return false;
+    }
+
+    arr2 = str.length;
+  }
+
+  return true;
 }
+
